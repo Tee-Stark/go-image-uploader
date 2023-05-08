ac624b68d727cb8dab1df87821ab1329da74a503
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-05-08 03:51:40 +0100

    [Update]Change ID from email to incrementing numbers

--- models/user.go
+++ models/user.go
@@ -1,23 +1,40 @@
 package models
 
 type User struct {
+	ID       int    `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	ImageUrl string `json:"image_url"`
 }
 
-var users map[string]User
+var users map[int]User
 
 func (u *User) CreateUser() {
-	users = make(map[string]User) // allocate memory to the map
+	users = make(map[int]User) // allocate memory to the map
 
-	users[u.Email] = *u
+	id := len(users) + 1
+	u.ID = id
+	users[id] = *u
 }
 
-func (u *User) GetUser(email string) User {
-	return users[email]
+func (u *User) GetUser(id int) User {
+	return users[id]
 }
 
-func (u *User) UpdateUser() {
-	users[u.Email] = *u
+func (u *User) UpdateUser(id int, update map[string]string) User {
+	user := users[id]
+
+	for key, value := range update {
+		switch key {
+		case "email":
+			user.Email = value
+		case "password":
+			user.Password = value
+		case "image_url":
+			user.ImageUrl = value
+		}
+	}
+
+	users[id] = user
+	return users[id]
 }
